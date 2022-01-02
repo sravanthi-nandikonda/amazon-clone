8239e39ee656363278e297751f938d2094ed3f93
sahana nandikonda <sravanthi.nandikonda@gmail.com>
2022-01-02 18:21:09 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,9 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
-//import Payment from "./Payment";
-//import Orders from "./Orders";
+import Payment from "./Payment";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
-//import { loadStripe } from "@stripe/stripe-js";
-//import { Elements } from "@stripe/react-stripe-js";
-
-// const promise = loadStripe(
-//   "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
-// );
-
 function App() {
   const [{}, dispatch] = useStateValue();
 
@@ -57,12 +49,10 @@ function App() {
             <Header />
             <Checkout />
           </Route>
-          {/* <Route path="/payment">
+          <Route path="/payment">
             <Header />
-            <Elements stripe={promise}>
               <Payment />
-            </Elements>
-          </Route> */}
+          </Route> 
           <Route path="/">
             <Header />
             <Home />
